507d5f995c3af0ebe3a704fa0d7f2229bdb4c678
bekir <bekir.karahodzic@outlook.com>
2020-10-04 16:50:56 +0200

    Updates

---
+++ precache-manifest.9d3c5ab9bd70d5640eada978086ee7a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4468c8ce492cb68cad569f49f3999e",
+    "url": "/pizzeria/index.html"
+  },
+  {
+    "revision": "501eddf21bbc17b8086b",
+    "url": "/pizzeria/static/css/main.24940465.chunk.css"
+  },
+  {
+    "revision": "58dd872905523972c8d5",
+    "url": "/pizzeria/static/js/2.ad7ad123.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizzeria/static/js/2.ad7ad123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501eddf21bbc17b8086b",
+    "url": "/pizzeria/static/js/main.4ead41c4.chunk.js"
+  },
+  {
+    "revision": "f7d23b4e1cde68efa72c",
+    "url": "/pizzeria/static/js/runtime-main.ce691bb0.js"
+  },
+  {
+    "revision": "400089f01106607c1d7b2f1978220dc8",
+    "url": "/pizzeria/static/media/pizza.400089f0.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/pizzeria/static/media/shopping-bag.30f07071.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pizzeria/precache-manifest.9d3c5ab9bd70d5640eada978086ee7a1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pizzeria/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
